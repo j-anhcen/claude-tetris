a521ad3144b68b782d3a5e05dfd2ce3f3d5d6253
jjancen <j.anhcen@gmail.com>
2026-05-10 22:36:39 -0500

    feat: add new Nut piece with corresponding colors

--- game.js
+++ game.js
@@ -13,6 +13,7 @@ const COLORS = [
   '#e57373', // Z - red
   '#90caf9', // J - pale blue
   '#ffb74d', // L - orange
+  '#b0bec5', // Nut - silver
 ];
 
 const COLORS_LIGHT = [
@@ -24,6 +25,7 @@ const COLORS_LIGHT = [
   '#dc2626', // Z - dark red
   '#2563eb', // J - blue
   '#ea580c', // L - dark orange
+  '#546e7a', // Nut - steel
 ];
 
 let activeColors = COLORS;
@@ -37,6 +39,7 @@ const PIECES = [
   [[5,5,0],[0,5,5],[0,0,0]],                  // Z
   [[6,0,0],[6,6,6],[0,0,0]],                  // J
   [[0,0,7],[7,7,7],[0,0,0]],                  // L
+  [[8,8,8],[8,0,8],[8,8,8]],                  // Nut
 ];
 
 const LINE_SCORES = [0, 100, 300, 500, 800];
@@ -60,7 +63,7 @@ function createBoard() {
 }
 
 function randomPiece() {
-  const type = Math.floor(Math.random() * 7) + 1;
+  const type = Math.floor(Math.random() * 8) + 1;
   const shape = PIECES[type].map(row => [...row]);
   return { type, shape, x: Math.floor(COLS / 2) - Math.floor(shape[0].length / 2), y: 0 };
 }
